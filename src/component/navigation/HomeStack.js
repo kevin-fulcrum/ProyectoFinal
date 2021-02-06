47dd9cd23f537979f7ea68c6ac4202127c1ce162
kevin-fulcrum <kevinariassistemas@gmail.com>
2021-02-06 01:02:42 -0500

    [feature]-create welcome

--- src/component/navigation/HomeStack.js
+++ src/component/navigation/HomeStack.js
@@ -1,47 +1,23 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Acceso from '../../screens/login/Acceso';
-import DatGeneral from '../../screens/inscripcion/DatGeneral';
+import Welcome from '../../screens/preVisualizacion/Welcome'
 import PreVisualizacion from '../../screens/preVisualizacion/PreVisualizacion'
-import DetallesCartPrincipal from '../../screens/preVisualizacion/DetallesCartPrincipal'
-import ProductDetalle from '../../screens/preVisualizacion/ProductDetalle'
-import Localitation from '../../screens/localitation/Localitation'
 
 const Stack = createStackNavigator();
 
 const HomeStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Acceso"
+      initialRouteName="Welcome"
       >
       <Stack.Screen
-        name="Acceso"
-        component={Acceso}
-        options={{header: () => null}}
-      />
-      <Stack.Screen
-        name="DatGeneral"
-        component={DatGeneral}
+        name="Welcome"
+        component={Welcome}
         options={{header: () => null}}
       />
       <Stack.Screen
         name="PreVisualizacion"
         component={PreVisualizacion}
-        options={{header: () => null}}
-      />
-       <Stack.Screen
-        name="DetallesCartPrincipal"
-        component={DetallesCartPrincipal}
-        options={{header: () => null}}
-      />
-       <Stack.Screen
-        name="ProductDetalle"
-        component={ProductDetalle}
-        options={{header: () => null}}
-      />
-       <Stack.Screen
-        name="Localitation"
-        component={Localitation}
         options={{header: () => null}}
       />
     </Stack.Navigator>
--- src/screens/preVisualizacion/Welcome.js
+++ src/screens/preVisualizacion/Welcome.js
@@ -3,14 +3,29 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  View,
 } from 'react-native';
+import Footer from '../../component/footer/Footer'
 
-const Welcome = () => {
+const Welcome = ({navigation}) => {
     const styles = StyleSheet.create({
-      
+      containerInit:{
+          flex: 0.9,
+      },
+      containerEnd:{
+          flex: 0.1,
+          alignItems: 'center'
+    }
       });  
   return (
-    <View></View>
+    <>
+    <View style={styles.containerInit}>
+        <Text>holas</Text>
+    </View>
+    <View style={styles.containerEnd}>
+        <Footer navigation={navigation}></Footer>
+    </View>
+    </>
   )
 };
 
