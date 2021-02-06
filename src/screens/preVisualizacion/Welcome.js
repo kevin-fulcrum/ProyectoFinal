1d961cd134f990c9db698de3640dfc7f981fcad1
kevin-fulcrum <kevinariassistemas@gmail.com>
2021-02-06 00:47:24 -0500

    [feature]-create welcome

---
+++ src/screens/preVisualizacion/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+const Welcome = () => {
+    const styles = StyleSheet.create({
+      
+      });  
+  return (
+    <View></View>
+  )
+};
+
+
+export default Welcome;
