c8e45303c0d0fd39d5959ea8659bfba04e4c93c5
kevin-fulcrum <kevinariassistemas@gmail.com>
2021-02-06 03:22:55 -0500

    [feacture]-fix

--- src/screens/detalle/DetalleProducto.js
+++ src/screens/detalle/DetalleProducto.js
@@ -8,7 +8,7 @@ import Button from '../../component/button/Button'
 const styles=StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#8cc2d1',
+        backgroundColor: '#d75971',
         width: windowWidth,
         height: windowHeight,
         justifyContent: 'flex-end'
@@ -44,27 +44,40 @@ const styles=StyleSheet.create({
     textoDetalle:{
         fontSize: 15,
         padding: 20
+    },
+    textoPrecio:{
+        fontSize: 20,
+        padding: 20,
+        fontWeight: 'bold'
     }
 })
 
-const DettalleProducto=(/*{navigation, route}*/)=>{
+const DettalleProducto=({navigation, route})=>{
   //const dispatch = useDispatch()
   /*const orderFun = async()=>{
     await dispatch(Action.setCartProduct(route.params))
     navigation.navigate('Cart',route.params)
   } */ 
+  console.warn('route',route.params)
   return(
     <View style={styles.container}>
         <View style={styles.image}>
         <Image
         style={{width:350, height:350}}
-        source={{uri: 'https://cdn.dribbble.com/users/3862493/screenshots/14876801/media/1d266532827556906e7ea57c80b62cf8.png?compress=1&resize=1000x750' /*route.params.url*/}}
+        source={{uri: 'data:image/png;base64,'+route.params.imagen}}
         />     
         </View>
     <View style={styles.detalle}>
         <View style={styles.datosDetalle}>
-        <Text style={styles.texto}>titulo{/*route.params.descripcion*/}                                                       precio</Text>
-        <Text style={styles.textoDetalle}>descripcion{/*route.params.detalle*/}</Text>
+        <View style={{flexDirection: 'row'}}>
+            <View style={{flex:0.5}}>
+            <Text style={styles.texto}>{route.params.titulo}</Text>
+            </View>   
+            <View style={{flex:0.5}}>
+            <Text style={styles.textoPrecio}>Precio: S/. {route.params.precio}</Text>
+            </View>
+        </View>
+        <Text style={styles.textoDetalle}>{route.params.descripcion}</Text>
         </View>
         <View style={styles.botones}>
         <Button label={'Order Now'} windowHeight={50} windowWidth={150} onPress={()=>{}}></Button>
